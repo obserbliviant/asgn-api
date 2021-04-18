d3e923aa8c91b272f17cf6b6c87c759964195be9
Dustin Simmons <obserbliviance@gmail.com>
2021-04-18 16:43:45 -0500

    Basic Express web server

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+
+let app = express();
+var port = 3000;
+
+app.get('/', (req, res) => res.send('Placeholder Get Response'));
+
+app.listen(port, function () {
+    console.log("Running asgn-api on port " + port);
+});
